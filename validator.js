4e6518d9b0d79bf2df2200de216532f9837b920a
unknown <0049003110@znv.com>
2021-04-25 15:43:33 +0800

    -

--- validator.js
+++ validator.js
@@ -2,15 +2,23 @@ import regular from './regular'
 
 const userCheck = function (rule, value, callback) {
   let reg = regular.special
-  if (reg.test(reg)) {
+  if (!reg.test(value)) {
     callback(new Error('用户名不能包含特殊字符'))
+  } else {
+    callback()
+  }
+}
+const passwordCheck = function (rule, value, callback) {
+  let reg = regular.twoPassword
+  if (!reg.test(value)) {
+    callback(new Error('密码至少含有字母、数字、特殊字符中的两种'))
   } else {
     callback()
   }
 }
 const cellphoneCheck = function (rule, value, callback) {
   let reg = regular.cellphone
-  if (reg.test(reg)) {
+  if (!reg.test(value)) {
     callback(new Error('非有效电话号码！'))
   } else {
     callback()
@@ -18,5 +26,6 @@ const cellphoneCheck = function (rule, value, callback) {
 }
 export {
   userCheck,
-  cellphoneCheck
+  cellphoneCheck,
+  passwordCheck
 }
